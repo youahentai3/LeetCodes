f3ab317781c635f43d1d0cf35cd735e6c3503202
youahentai <z1654026163@163.com>
2020-07-26 18:47:59 +0800

    999

--- offer5.cpp
+++ offer5.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <vector>
+#include <climits>
+#include <deque>
 
 using namespace std;
 
@@ -62,9 +64,105 @@ public:
 
         return re;
     }
+
+    void reverse_string(string& s,int p,int r)
+    {
+        while(p<r)
+        {
+            char te=s[p];
+            s[p]=s[r];
+            s[r]=te;
+            p++;
+            r--;
+        }
+        //cout<<s<<endl;
+    }
+
+    string reverseWords(string s) 
+    {
+        reverse_string(s,0,s.size()-1);
+        //cout<<s<<endl;
+        int p=0;
+        for(int i=0;i<s.size();i++)
+        {
+            if(s[i]==' ')
+            {
+                reverse_string(s,p,i-1);
+                p=i+1;
+            }
+            else if(i==s.size()-1)
+                reverse_string(s,p,i);
+        }
+        string str="";
+        for(auto ite=s.begin();ite!=s.end();)
+        {
+            if(str.empty() && *ite==' ')
+            {
+                ite++;
+                continue;
+            }
+            if(*ite==' ')
+            {
+                while(ite!=s.end() && *ite==' ')
+                    ite++;
+                if(ite==s.end())
+                    break;
+                str+=' ';
+            }
+            else 
+            {
+                str+=*ite;
+                ite++;
+            }
+        }
+
+        return str;
+    }
+
+    string reverseLeftWords(string s, int n) 
+    {
+        reverse_string(s,0,n-1);
+        reverse_string(s,n,s.size()-1);
+        reverse_string(s,0,s.size()-1);
+        return s;
+    }
+
+    vector<int> maxSlidingWindow(vector<int>& nums, int k) 
+    {
+        vector<int> vec;
+        if(nums.empty() || nums.size()<k)
+            return vec;
+        deque<int> index_que;
+        for(int i=0;i<k-1;i++)
+        {
+            while(!index_que.empty() && nums[index_que.back()]<nums[i])
+            {
+                index_que.pop_back();
+            }
+            index_que.push_back(i);
+        }
+        for(int i=k-1;i<nums.size();i++)
+        {
+            while(!index_que.empty() && nums[index_que.back()]<nums[i])
+            {
+                index_que.pop_back();
+            }
+            index_que.push_back(i);
+            if(index_que.back()-index_que.front()>=k)
+                index_que.pop_front();
+            vec.push_back(nums[index_que.front()]);
+        }
+
+        return vec;
+    }
 };
 
 int main()
 {
+    string s;
+    getline(cin,s);
+    Solution solution;
+    cout<<solution.reverseWords(s)<<endl;
+
     return 0;
 }
