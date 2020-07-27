a74e642d1fceb0fc8e01d84018a23a48a96d4858
youahentai <z1654026163@163.com>
2020-07-27 20:00:19 +0800

    987

--- offer5.cpp
+++ offer5.cpp
@@ -2,8 +2,50 @@
 #include <vector>
 #include <climits>
 #include <deque>
+#include <string>
+#include <queue>
 
 using namespace std;
+
+class MaxQueue 
+{
+private:
+    deque<int> deq;
+    queue<int> que;
+    int index=0;
+public:
+    MaxQueue() {}
+    
+    int max_value() 
+    {
+        if(deq.empty())
+            return -1;
+        return deq.front();
+    }
+    
+    void push_back(int value) 
+    {
+        while(!deq.empty() && deq.back()<=value)
+        {
+            deq.pop_back();
+            index++;
+        }
+        deq.push_back(value);
+        que.push(value);
+    }
+    
+    int pop_front()
+    {
+        if(deq.empty())
+            return -1;
+        int te=que.front();
+        if(te==deq.front())
+            deq.pop_front();
+        que.pop();
+
+        return te;
+    }
+};
 
 class Solution
 {
@@ -154,6 +196,71 @@ public:
         }
 
         return vec;
+    }
+
+    vector<double> twoSum(int n) 
+    {
+        vector<double> re;
+        vector<vector<double>> vec;
+        vec.push_back(vector<double>(n*6+1));
+        vec.push_back(vector<double>(n*6+1));
+        int cur=0;
+
+        for(int i=1;i<=6;i++)
+            vec[cur][i]=1;
+        cur=!cur;
+
+        for(int i=1;i<n;i++)
+        {
+            for(int j=1;j<=6*n;j++)
+            {
+                vec[cur][j]=0;
+                for(int k=1;k<=6;k++)
+                {
+                    if(k>j)
+                        break;
+                    vec[cur][j]+=vec[!cur][j-k];
+                }
+            }
+            cur=!cur;
+        }
+
+        double total=pow(6,n);
+        for(int i=1;i<vec[!cur].size();i++)
+        {
+            if(vec[!cur][i])
+                re.push_back((vec[!cur][i]/total));
+        }
+
+        return re;
+    }
+
+    bool isStraight(vector<int>& nums) 
+    {
+        sort(nums.begin(),nums.end());
+        int cou=0;
+        for(auto ite=nums.begin();ite!=nums.end();ite++)
+        {
+            if(!(*ite))
+                cou++;
+            else 
+            {
+                if(ite+1!=nums.end())
+                {                    
+                    if(*(ite+1)==*ite)
+                        return false;
+                    int gap=*(ite+1)-(*ite);
+                    if(gap>cou)
+                    {
+                        return false;
+                    }
+                    else 
+                        cou-=gap;
+                }
+            }
+        }
+
+        return true;
     }
 };
 
